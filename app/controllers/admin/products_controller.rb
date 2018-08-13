9480414e9620f290e686d92f4c4f438632fff847
vita <dragic.hsieh@gmail.com>
2018-08-13 22:40:12 +0800

    Done add product new and create actions and views for admin/mains

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,26 @@
 class Admin::ProductsController < ApplicationController
 	def index
 		@products = Product.all
-	end 
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+			flash[:notice] = "產品建立成功"
+			redirect_to admin_products_path
+		else
+			flash.now[:alert] = "產品建立失敗，是不是少了什麼 !?"
+			render :new
+		end
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :product_price, :description)
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+	validates :name, :description, :product_price, presence: true
 end
