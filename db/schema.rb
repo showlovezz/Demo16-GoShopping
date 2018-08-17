5c0fc88f57e3ac71a3ac5f332b72f1a767092009
vita <dragic.hsieh@gmail.com>
2018-08-18 01:38:16 +0800

    Done create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_022949) do
+ActiveRecord::Schema.define(version: 2018_08_17_173649) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_08_14_022949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "product_image"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
