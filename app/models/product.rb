db0f80149172cb900ea01542a22fe7f082a6ef2a
vita <dragic.hsieh@gmail.com>
2018-08-18 01:57:43 +0800

    Done link category and product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
 	validates :name, :description, :product_price, presence: true
 
 	mount_uploader :product_image, PhotoUploader
+
+	belongs_to :category, optional: true
 end
