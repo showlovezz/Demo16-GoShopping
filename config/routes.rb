d717737dc0106fbc80faa98bdcf6ff6029d549a3
vita <dragic.hsieh@gmail.com>
2018-08-23 18:35:04 +0800

    Done setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "mains#index" # Front-end index
-  resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :products, only: [:index, :show] do # Front-end for products
+    resources :comments, only: [:create, :destroy] # Front-end for comments
+  end
+  resources :categories, only: [:show] # Front-end for categories
 
   namespace :admin, path: "mains" do
   	root "mains#index" # Back-end index
