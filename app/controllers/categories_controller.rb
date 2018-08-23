ee940071ef9d7635708ae4d0c1e7b2308d48fe7d
vita <dragic.hsieh@gmail.com>
2018-08-23 17:53:57 +0800

    Done user on index page can select products by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,8 @@
 class CategoriesController < ApplicationController
+
+	def show
+		@categories = Category.all
+		@category = Category.find(params[:id])
+		@products = @category.products.page(params[:page]).per(12)
+	end
 end
