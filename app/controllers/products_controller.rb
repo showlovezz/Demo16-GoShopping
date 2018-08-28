351e9f0fcd3ca1022f1e5106d08a699410c788ef
vita <dragic.hsieh@gmail.com>
2018-08-28 19:49:00 +0800

    Done add random_product on product/show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
+		@random_product = Product.where.not(id: @product.id).order("RANDOM()").limit(3)
 	end
 end
